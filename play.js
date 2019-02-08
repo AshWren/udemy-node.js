a482ed329c7c540455c48a7e44cbdb25511aada9
DESKTOP-SGAB5U8\Rachel <rachelstuckey@yahoo.com>
2019-02-08 13:56:40 -0600

    first real commit

---
+++ play.js
@@ -0,0 +1,70 @@
+setTimeout(() => {
+    console.log('Timer is done!')
+}, 2000);
+
+
+
+
+
+// const person = {
+//     name: 'Max',
+//     age: 29,
+//     greet() {
+//         console.log('Hi, I am ' + this.name);
+//     }
+// };
+
+
+// const printName = ({ name }) => {
+//     console.log(name);
+// }
+
+// printName(person);
+
+// const { name, age } = person;
+
+// // person.greet();
+
+// const hobbies = ['Sports', 'Cooking'];
+// const [hobby1, hobby2] = hobbies;
+// console.log(hobby1, hobby2);
+
+// // for(let hobby of hobbies) {
+// //     console.log(hobby);
+// // }
+
+// // console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// // console.log(hobbies);
+
+// const copiedPerson = {...person};
+// console.log(copiedPerson)
+
+// const copiedArray = [...hobbies];
+// console.log(copiedArray)
+
+// const toArray = (...args) => {
+//     return args;
+// }
+
+// console.log(toArray(1,2,3,4));
+
+// const name = 'Max';
+// let age = 29;
+// const hasHobbies = true;
+
+// age = 30;
+
+// const summarizeUser = (userName, userAge, userHasHobby) => {
+//     return (
+//         'Name is ' +
+//         userName +
+//         ', age is ' + 
+//         userAge +
+//         ' and the user has hobbies: ' + 
+//         userHasHobby
+//     ); 
+// }
+
+// const add = (a,b) => a + b;
+// const addOne = a => a + 1;
+// const addRandom = () => 1 + 2;
